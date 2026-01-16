019c2a20d6d4e0676a06c79001a15f0934869509
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-01-16 13:47:43 -0600

    Delete netstr.ps1

--- netstr.ps1
+++
@@ -1,5 +0,0 @@
-$netstr = (netsh wlan show interfaces) -match '^\s+Signal' -replace '^/s+Signal\s+:',''
-
-while (1) {
-    write-host $netstr -foregroundcolor cyan | format-table; sleep 1; clear
-}
