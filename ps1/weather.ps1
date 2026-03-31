69d7fa5fd6cf5938314587e0072c2bcb61b6fa37
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-03-31 08:52:01 -0500

    Update weather.ps1

--- ps1/weather.ps1
+++ ps1/weather.ps1
@@ -1,11 +1,17 @@
 #get user input and put it into the url for wttr.in
-$weather = read-host 'Please type the city you wish to check'
+
+param(
+    [parameter(Mandatory=$true)]
+    [string]$loc
+)
+
+#$weather = read-host 'Please type the city you wish to check'
 $part1 = 'http://wttr.in/'
 $part2 = '?format=3'
 
-(curl ($part1 + $weather + $part2) -useragent 'curl').content
+(curl ($part1 + $loc + $part2) -useragent 'curl').content
 
-$asdf = (curl ($part1 + $weather + $part2) -useragent 'curl').content
+$asdf = (curl ($part1 + $loc + $part2) -useragent 'curl').content
 
 add-type -assemblyname system.speech
 $speak = new-object system.speech.synthesis.speechsynthesizer
