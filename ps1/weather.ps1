4ed4906ed097ee0c5267bceb2292f2116acfe283
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-04-03 07:21:04 -0500

    Update weather.ps1
    Updated script to check for OS to avoid useragent error

--- ps1/weather.ps1
+++ ps1/weather.ps1
@@ -9,11 +9,18 @@ param(
 $part1 = 'http://wttr.in/'
 $part2 = '?format=3'
 
-(curl ($part1 + $loc + $part2) -useragent 'curl').content
+#OS check, Unix does not like the useragent flag
+if ($IsWindows) {
+   (curl ($part1 + $loc + $part2) -useragent 'curl').content
 
-$asdf = (curl ($part1 + $loc + $part2) -useragent 'curl').content
+    $asdf = (curl ($part1 + $loc + $part2) -useragent 'curl').content
 
-add-type -assemblyname system.speech
-$speak = new-object system.speech.synthesis.speechsynthesizer
+    add-type -assemblyname system.speech
+    $speak = new-object system.speech.synthesis.speechsynthesizer
 
-$speak.speak($asdf)
+    $speak.speak($asdf) 
+}
+
+else {
+    curl ($part1 + $loc + $part2)
+}
