2d056be9b0d837860ad56cf0ef2e1b10bf2a7372
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-06-01 06:57:33 -0500

    updated top.ps1 to include last boot time

--- ps1/top.ps1
+++ ps1/top.ps1
@@ -1,9 +1,14 @@
 #re-creating the 'top' command from linux environments
 
-While(1) 
-	{
+function get-top {
 	gps | 
 	sort-object -des cpu | 
 	select -f 15 |
 	format-table ; sleep 1; clear
+}
+
+$uptm = get-uptime -Since
+
+while(1) {
+	write-host "System was last booted: $uptm" | get-top
 }
