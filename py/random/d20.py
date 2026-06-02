c1a19fbf623a95f4f3240cfd64a3c5674ec17650
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-06-02 11:13:23 -0500

    updated d20.py

--- py/random/d20.py
+++ py/random/d20.py
@@ -1,8 +1,50 @@
-import random
+import random,time,os
+
+life = 0
+death = 0
+
+print("Starting death save...")
+time.sleep(2)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#color text
+CRED = '\033[91m'
+CGREEN = '\033[92m'
+CYELLOW = '\033[93m'
+CBLUE = '\033[94m'
+CEND = '\033[0m'
 
 def dice():
+    global life
+    global death
     d20 = random.randint(1,20)
-    print(d20)
+
+    #life +1
+    if (10 <= d20 <= 19):
+        life += 1
+
+    #nat20 life +2
+    elif (d20 == 20):
+        life += 2
+
+    #nat1 life -2
+    elif (d20 == 1):
+        death += 2
+
+    #life -1
+    elif (2 <= d20 <= 9):
+        death += 1
+
+    print(CYELLOW + str(d20) + CEND)
 
 for x in range(5):
     dice()
+    #print("Life: " + str(life) + " / Death: " + str(death))
+    time.sleep(1)
+
+if (life < 3):
+    print(CRED + "You died" + CEND)
+else:
+    print(CGREEN + "You lived!" + CEND)
+
+life = 0
