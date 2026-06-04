37594f1c8d13bde89aac116a277aa16586e07923
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-06-04 06:57:51 -0500

    updated pacs.py to include host IP info  for easier digestability

--- py/network/pacs.py
+++ py/network/pacs.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse,time,subprocess
 from scapy.all import sniff, IP
 
 def main():
@@ -11,7 +11,12 @@ def main():
 
     sniff(filter=args.filter, iface=args.interface, count=args.count,prn=lambda x:x.summary())
 
+IP_info = subprocess.check_output(['hostname', '-I']).decode('utf-8').strip()
+
+#printing host IP to make dump easier to digest
+print("IP info: ", IP_info)
 print("Starting scan...")
+time.sleep(1)
 
 if __name__ == "__main__":
     main()
