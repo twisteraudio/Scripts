61c595981e2fa1225cb540a058c2c6a4deb7614c
TwisterAudio <54367059+twisteraudio@users.noreply.github.com>
2026-08-05 10:04:26 -0500

    created fileath argument for rcran.py in py/repy updated readme

--- py/repy/README.md
+++ py/repy/README.md
@@ -6,10 +6,25 @@ This folder may be updated as new features or ideas come around.
 
 # Contents
 
-rcref.xlsx - xlsx of record collection
-rcran.py - python script to parse through rcref.xlsx
-    -c - returns a count of random selections from spreadsheet
-    -q - queries the spreadsheet using Pandas' query function
+rcref.xlsx      - xlsx of record collection
+rcran.py        - python script to parse through rcref.xlsx
+    -p, --path  - [Required] path to file
+    -c, --count - returns a count of random selections from spreadsheet
+    -q, --query - queries the spreadsheet using Pandas' query function
+
+# Examples
+
+    Select 10 random records from collection
+    python3 -p '/path/to/rcref.xlsx' -c 10
+
+    Search for Artist Foo Fighters
+    python3 -p '/path/to/rcref.xlsx' -q "Artist == 'Foo Fighters'"
+
+    Search for Albums with 'Best' in the title
+    python3 -p '/path/to/rcref.xlsx' -q "Album.str.contains('Best')"
+
+    Search for most recent entry to collection
+    python3 -p '/path/to/rcref.xlsx' -q "Timestamp == Timestamp.max()"
 
 # [Disclaimer]
 
--- py/repy/rcran.py
+++ py/repy/rcran.py
@@ -5,75 +5,91 @@ Outputs random selection of rows from record collection spreadsheet
 Reason: I wanted to have fun with record collection and use python to randomly select records to listen to
 
 Usage:
-    python3 rcran.py    - default (outputs 3 selections)
-    python3 rcran.py -c 5   - outputs X count selections
-    python3 rcran.py -q SOME_QUERY  - query the spreadsheet
+    python3 rcran.py                                       - default (outputs 3 selections)
+    python3 rcran.py -p '/path/to/file' -c 5               - outputs X count selections
+    python3 rcran.py -p '/path/to/file' -q "SOME_QUERY"    - query the spreadsheet
 """
+
 import pandas as pd
-import os
-import time
-import sys
-import subprocess
-import argparse
+import os, time, sys, subprocess, argparse
 
 parser = argparse.ArgumentParser(description = "Record Collection Parser")
 parser.add_argument("-c", "--count", type=int, help="Number of entries")
 parser.add_argument("-q", "--query", type=str, help="Query Spreadsheet")
+parser.add_argument("-p", "--path", type=str, required=True, help="Path to file")
 args = parser.parse_args()
 
 #color text
-CRED = '\033[91m'
-CGREEN = '\033[92m'
-CYELLOW = '\033[93m'
-CBLUE = '\033[94m'
-CEND = '\033[0m'
+class Color:
 
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    LIGHTGRAY = '\033[37m'
+    GRAY = '\033[90m'
+    LIGHTRED = '\033[91m'
+    LIGHTGREEN = '\033[92m'
+    LIGHTYELLOW = '\033[93m'
+    LIGHTBLUE = '\033[94m'
+    LIGHTMAGENTA = '\033[95m'
+    LIGHTCYAN = '\033[96m'
+    WHITE = '\033[97m'
+    BOLD = '\033[1m'
+    ITALIC = '\033[3m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
 
 def clear_scn():
+
     subprocess.run(['cls'] if os.name == 'nt' else ['clear'], shell=os.name == 'nt')
 
 #loading screen
 def load_scn():
+
     clear_scn()
-    print(CYELLOW + "Loading selection..." + CEND)
+    print(f"{Color.YELLOW}Loading selection...{Color.END}")
     time.sleep(1)
 
 #select random selections
 def pd_sample():
 
-    df = pd.read_excel('recol.xlsx', usecols = 'B:C')
-    print(CGREEN + "*" * 75 + CEND)
-    print(CGREEN + df.sample(args.count) + CEND)
-    print(CGREEN + "*" * 75 + CEND)
+    df = pd.read_excel(args.path, usecols = 'B:C')
+    print(f"{Color.GREEN}*{Color.END}" * 75)
+    print(f"{Color.LIGHTCYAN}{df.sample(args.count).to_string(index=False)}{Color.END}")
+    print(f"{Color.GREEN}*{Color.END}" * 75)
 
 #query spreadsheet
 #df.query()
 def pd_query():
 
-    df = pd.read_excel('recol.xlsx', usecols = 'A:E')
+    df = pd.read_excel(args.path, usecols = 'A:E')
     try:
-        query = df.query(args.query)
+        query = df.query(args.query).to_string(index=False)
         
-        print(query)
+        print(f"{Color.LIGHTCYAN}{query}{Color.END}")
 
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
 
 def main():
+
     if args.count:
         try:
             load_scn()
             pd_sample()
         finally:
-            print(CGREEN + "Selection Complete" + CEND)
+            print(f"{Color.GREEN}Selection Complete{Color.END}")
 
     if args.query:
         try:
             load_scn()
             pd_query()
         finally:
-            print(CGREEN + "Selection Complete" + CEND)
+            print(f"{Color.GREEN}Selection Complete{Color.END}")
 
 if __name__ == "__main__":
     main()
